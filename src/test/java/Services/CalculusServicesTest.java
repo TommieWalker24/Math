3a18156936c70160b66b722a8e9a594d5f1ae7d0
Tommie <57117928+TommieWalker@users.noreply.github.com>
2020-01-28 10:53:19 -0600

    Completed fraction functionality

--- src/test/java/Services/CalculusServicesTest.java
+++ src/test/java/Services/CalculusServicesTest.java
@@ -1,11 +1,17 @@
 package Services;
 
 import Entities.ImaginaryNumber;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculusServicesTest extends TrigServices {
+    private CalculusServices services;
+    @Before
+    public void init(){
+        services = new CalculusServices();
+    }
 
     @Test
     void testDerivative() {
@@ -14,7 +20,7 @@ class CalculusServicesTest extends TrigServices {
         imaginaryNumber.setVariable("x");
         imaginaryNumber.setExponent(3);
 
-        ImaginaryNumber result = CalculusServices.derivative(imaginaryNumber);
+        ImaginaryNumber result = services.derivative(imaginaryNumber);
 
         assertEquals(15, result.getRealNumber());
         assertEquals("x", result.getVariable());
@@ -25,7 +31,7 @@ class CalculusServicesTest extends TrigServices {
         imaginaryNumber.setVariable("x");
         imaginaryNumber.setExponent(1);
 
-        ImaginaryNumber result2 = CalculusServices.derivative(imaginaryNumber);
+        ImaginaryNumber result2 = services.derivative(imaginaryNumber);
         assertEquals(3, result2.getRealNumber());
         assertEquals("no variable", result2.getVariable());
         assertEquals(0, result2.getExponent());
